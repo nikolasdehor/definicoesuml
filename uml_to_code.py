291c127977cbf345b25c215832788289a831c16d
Nikolas de Hor <madeira_clerigo_0@icloud.com>
2023-11-22 21:40:18 -0300

    update

--- uml_to_code.py
+++ uml_to_code.py
@@ -1,80 +1,30 @@
-import tkinter as tk
-from pyparsing import Word, alphas, Group, ZeroOrMore, Suppress, Optional, oneOf
-import unittest
+def carregar_definicao(arquivo):
+    with open(arquivo, 'r') as f:
+        definicao = f.read()
+    return definicao
 
-# Classe para representar uma UML Class
-class UMLClass:
-    def __init__(self, name):
-        self.name = name
-        self.attributes = []
-        self.methods = []
+def gerar_codigo(definicao):
+    codigo = []
+    for classe in definicao.split('==========================='):
+        if not classe.strip():
+            continue
+        classe_nome = classe.split('\n')[0].strip()
+        atributos = classe.split('\n')[1:-2]
+        metodos = classe.split('\n')[-2:-1][0].split('+ ')
+        codigo.append(f'public class {classe_nome} {{')
+        for atributo in atributos:
+            tipo, nome = atributo.split(': ')
+            codigo.append(f'    private {tipo} {nome};')
+        for metodo in metodos:
+            codigo.append(f'    public {metodo.strip()}(){{}}')
+        codigo.append('}')
+    return '\n'.join(codigo)
 
-    def add_attribute(self, attr):
-        self.attributes.append(attr)
-
-    def add_method(self, method):
-        self.methods.append(method)
-
-# Classe para gerar código
-class CodeGenerator:
-    def generate_class(self, uml_class):
-        code = f"class {uml_class.name}:\n"
-        for attr in uml_class.attributes:
-            code += f"    {attr[0]}: {attr[1]}\n"
-        for method in uml_class.methods:
-            code += f"    def {method[0]}(self):\n        pass\n"
-        return code
-
-# Função para analisar UML
-def parse_uml(text):
-    identifier = Word(alphas)
-    attribute = Group(identifier + Suppress(":") + identifier)
-    method = Group(identifier + Suppress("()"))
-    classDef = Group(identifier + Suppress("{") + ZeroOrMore(attribute | method) + Suppress("}"))
-
-    parsedResult = classDef.parseString(text)
-    uml_class = UMLClass(parsedResult[0])
-    for item in parsedResult[1:]:
-        if len(item) == 2:
-            uml_class.add_attribute(item)
-        else:
-            uml_class.add_method(item)
-    return uml_class
-
-# Interface Gráfica
-def generate_code():
-    uml_text = text_input.get("1.0", "end-1c")
-    uml_class = parse_uml(uml_text)
-    code = code_generator.generate_class(uml_class)
-    code_output.delete("1.0", "end")
-    code_output.insert("1.0", code)
-
-root = tk.Tk()
-root.title("UML to Code Generator")
-
-text_input = tk.Text(root, height=10)
-text_input.pack()
-
-generate_button = tk.Button(root, text="Generate", command=generate_code)
-generate_button.pack()
-
-code_output = tk.Text(root, height=10)
-code_output.pack()
-
-code_generator = CodeGenerator()
-
-root.mainloop()
-
-# Testes Unitários
-class TestCodeGeneration(unittest.TestCase):
-    def test_class_generation(self):
-        uml_class = UMLClass("Test")
-        uml_class.add_attribute(("attr1", "int"))
-        uml_class.add_method(("method1",))
-        code = code_generator.generate_class(uml_class)
-        self.assertIn("class Test:", code)
-        self.assertIn("attr1: int", code)
-        self.assertIn("def method1(self):", code)
+def salvar_codigo(codigo, arquivo):
+    with open(arquivo, 'w') as f:
+        f.write(codigo)
 
 if __name__ == '__main__':
-    unittest.main()
+    definicao = carregar_definicao('definicao.txt')
+    codigo = gerar_codigo(definicao)
+    salvar_codigo(codigo, 'codigo_gerado.java')
